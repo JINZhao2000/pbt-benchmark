1e0dcd4db38a308e07851af10678a430cba29783
Zhao JIN <zhao.jin@ensiie.fr>
2023-05-27 22:05:24 +0200

    update scc neo4j

--- src/main/java/cyou/zhaojin/Application.java
+++ src/main/java/cyou/zhaojin/Application.java
@@ -3,13 +3,13 @@ package cyou.zhaojin;
 import cyou.zhaojin.graph.ImportUtil;
 import cyou.zhaojin.obj.Edge;
 import cyou.zhaojin.obj.Vertex;
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
-import org.neo4j.driver.Session;
+import org.neo4j.driver.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Zhao JIN
@@ -18,18 +18,120 @@ public class Application {
     public static final String VERTEX = "txn";
     public static final String EDGE = "dep";
     public static final String POSTFIX = ".json";
-    public static int NUM = 50;
 
     public static final Driver driver = GraphDatabase.driver("bolt://127.0.0.1:7687", AuthTokens.basic("neo4j", "12345678"));
+    public final static String serializable = "match (n:txn) with collect(n) as nodes call apoc.nodes.cycles(nodes) yield path return path";
+    public final static Pattern pattern = Pattern.compile(":(\\w{2})");
+
+    public final static String rw = "rw";
+    public static final String pl2 = "match (n:txn) with collect(n) as nodes call apoc.nodes.cycles(nodes, {relTypes: ['ww','wr']}) yield path return path";
+    public static final String pl1 = "match (n:txn) with collect(n) as nodes call apoc.nodes.cycles(nodes, {relTypes: ['ww']}) yield path return path";
+
+    public static final String scc = "call gds.alpha.scc.write('pbt', {}) yield maxSetSize as s return s";
+
+    public static boolean SerTest() throws Exception {
+        try (Session session = driver.session()) {
+            Result result = session.run(serializable);
+            return !result.hasNext();
+        }
+    }
+
+    public static boolean SITest() throws Exception{
+        try(Session session = driver.session()) {
+            Result result = session.run(serializable);
+            while (result.hasNext()) {
+                Record next = result.next();
+                String res = next.get("path").toString();
+                Matcher matcher = pattern.matcher(res);
+                List<String> cycle = new ArrayList<>();
+                while (matcher.find())
+                    cycle.add(matcher.group(1));
+                boolean findRW = false;
+                for (int i = 1; i < cycle.size(); i++) {
+                    if (cycle.get(i).equals(rw) && cycle.get(i).equals(rw)) {
+                        findRW = true;
+                        break;
+                    }
+                }
+                if (!findRW) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static boolean PSITest() throws Exception{
+        // the kernel of neo4j is java
+        try(Session session = driver.session()) {
+            Result result = session.run(serializable);
+            while (result.hasNext()) {
+                Record next = result.next();
+                String res = next.get("path").toString();
+                Matcher matcher = pattern.matcher(res);
+                List<String> cycle = new ArrayList<>();
+                while (matcher.find())
+                    cycle.add(matcher.group(1));
+                if (cycle.stream().filter(rw::equals).count() < 2)
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public static boolean PL2Test() throws Exception {
+        try (Session session = driver.session()) {
+            Result result = session.run(pl2);
+            return !result.hasNext();
+        }
+    }
+
+    public static boolean PL1Test() throws Exception {
+        try (Session session = driver.session()){
+            Result result = session.run(pl1);
+            return !result.hasNext();
+        }
+    }
+
+    public static boolean SCCTest() throws Exception {
+        try (Session session = driver.session()){
+            Result result = session.run(scc);
+            return result.next().get(0).asInt() == 1;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-        importGraph(args[0], NUM);
+        String[] nameSet = new String[]{"list_append"};
+        for (String t : nameSet) {
+            System.out.println(t);
+            for (int j = 0; j < 10; j++) {
+                for (int i = 10; i <= 50; i+=10) {
+                    importGraph(t, i);
+                }
+                System.out.println();
+            }
+//            for (int i = 10; i <= 50; i+=10) {
+//                importGraph(t, i);
+//                String res = t + " " + i + ":\t" +
+//                        SerTest() + "\t" +
+////                        SCCTest();
+//                        SITest() + "\t" +
+//                        PSITest() + "\t" +
+//                        PL2Test() + "\t" +
+//                        PL1Test();
+//                System.out.println(res);
+//            }
+        }
     }
 
     public static void importGraph(String type, int num) throws IOException {
         List<Vertex> vertices = ImportUtil.getVertex(type + "/" + VERTEX + num + POSTFIX);
         List<Edge> edges = ImportUtil.getEdge(type + "/" + EDGE + num + POSTFIX);
         try (Session session = driver.session()){
+            Result hasGDSGraph = session.run("call gds.graph.exists('pbt')");
+                if (hasGDSGraph.next().get("exists").isTrue()) {
+                    session.run("call gds.graph.drop('pbt')");
+                }
             session.run("match (n) detach delete (n)");
             StringBuilder vertex = new StringBuilder("create ");
             for (int i = 0; i < vertices.size(); i++) {
@@ -41,6 +143,7 @@ public class Application {
                 String edge = "match (n1:txn), (n2:txn) where n1.`_id`=\""+e.get_from()+"\" and n2.`_id`=\""+e.get_to()+"\" create (n1)-[:"+e.getType()+"{from: '"+e.getFrom_evt()+"', to: '"+e.getTo_evt()+"'}]->(n2)";
                 session.run(edge);
             });
+            session.run("CALL gds.graph.project.cypher( 'pbt', 'MATCH (n:txn) RETURN id(n) AS id', 'MATCH (n:txn)-->(n2:txn) RETURN id(n) AS source, id(n2) AS target')");
         }
     }
 }
--- src/test/java/cyou/zhaojin/CypherBenchmarkTest.java
+++ src/test/java/cyou/zhaojin/CypherBenchmarkTest.java
@@ -1,5 +1,6 @@
 package cyou.zhaojin;
 
+import org.junit.Test;
 import org.neo4j.driver.*;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.results.RunResult;
@@ -30,15 +31,22 @@ public class CypherBenchmarkTest {
     public final static String rw = "rw";
     public static final String pl2 = "match (n:txn) with collect(n) as nodes call apoc.nodes.cycles(nodes, {relTypes: ['ww','wr']}) yield path return path";
     public static final String pl1 = "match (n:txn) with collect(n) as nodes call apoc.nodes.cycles(nodes, {relTypes: ['ww']}) yield path return path";
+    public static final String dropproj = "call gds.graph.drop('pbt')";
+    public static final String scc = "call gds.alpha.scc.write('pbt', {}) yield maxSetSize as s return s";
+    public static final String serproj = "CALL gds.graph.project.cypher( 'pbt', 'MATCH (n:txn) RETURN id(n) AS id', 'MATCH (n:txn)-->(n2:txn) RETURN id(n) AS source, id(n2) AS target')";
+    public static final String pl2proj = "CALL gds.graph.project.cypher( 'pbt', 'MATCH (n:txn) RETURN id(n) AS id', 'MATCH (n:txn)-[:ww|wr]->(n2:txn) RETURN id(n) AS source, id(n2) AS target')";
+    public static final String pl1proj = "CALL gds.graph.project.cypher( 'pbt', 'MATCH (n:txn) RETURN id(n) AS id', 'MATCH (n:txn)-[:ww]->(n2:txn) RETURN id(n) AS source, id(n2) AS target')";
+    public static final String sccstream = "CALL gds.alpha.scc.stream('pbt', {}) YIELD nodeId, componentId WITH componentId, COLLECT(nodeId) AS nodes, COUNT(nodeId) AS num WHERE num > 1 RETURN nodes";
+    public static final String rlist = "match (n1:txn)-[r]->(n2:txn) where id(n1) in %s and id(n2) in %s with collect(type(r)) as r return r";
 
-    @Benchmark
+//    @Benchmark
     public void SerTest() throws Exception {
         try (Session session = driver.session()) {
             Result result = session.run(serializable);
         }
     }
 
-    @Benchmark
+//    @Benchmark
     public void SITest() throws Exception{
         try(Session session = driver.session()) {
             Result result = session.run(serializable);
@@ -63,8 +71,9 @@ public class CypherBenchmarkTest {
         }
     }
 
-    @Benchmark
+//    @Benchmark
     public void PSITest() throws Exception{
+        // the kernel of neo4j is java
         try(Session session = driver.session()) {
             Result result = session.run(serializable);
             while (result.hasNext()) {
@@ -80,18 +89,105 @@ public class CypherBenchmarkTest {
         }
     }
 
-    @Benchmark
+//    @Benchmark
     public void PL2Test() throws Exception {
         try (Session session = driver.session()) {
             Result result = session.run(pl2);
         }
     }
 
-    @Benchmark
+//    @Benchmark
     public void PL1Test() throws Exception {
         try (Session session = driver.session()){
             Result result = session.run(pl1);
         }
+    }
+
+    @Benchmark
+    public void Q1SerProjTest() throws Exception {
+        try (Session session = driver.session()){
+            session.run(dropproj);
+            session.run(serproj);
+        }
+    }
+
+    @Benchmark
+    public void Q2SerSCCTest() throws Exception {
+        try (Session session = driver.session()){
+            Result result = session.run(scc);
+        }
+    }
+
+    @Benchmark
+    @Test
+    public void Q3_SISCCTest() throws Exception {
+        try (Session session = driver.session()){
+            Result result = session.run(sccstream);
+            while (result.hasNext()) {
+                String list = result.next().get(0).toString();
+                Result innerResult = session.run(String.format(rlist, list, list));
+                List<String> cycle = innerResult.next().get(0).asList(Value::asString);
+                boolean findRW = false;
+                for (int i = 1; i < cycle.size(); i++) {
+                    if (cycle.get(i).equals(rw) && cycle.get(i).equals(rw)) {
+                        findRW = true;
+                        break;
+                    }
+                }
+                if (!findRW) {
+                    break;
+                }
+            }
+        }
+    }
+
+    @Benchmark
+    public void Q4PSISCCTest() throws Exception {
+        try (Session session = driver.session()){
+            Result result = session.run(sccstream);
+            while (result.hasNext()) {
+                String list = result.next().get(0).toString();
+                Result innerResult = session.run(String.format(rlist, list, list));
+                List<String> cycle = innerResult.next().get(0).asList(Value::asString);
+                if (cycle.stream().filter(rw::equals).count() < 2)
+                    break;
+            }
+        }
+    }
+
+    @Benchmark
+    public void Q5PL2ProjTest() throws Exception {
+        try (Session session = driver.session()){
+            session.run(dropproj);
+            session.run(pl2proj);
+        }
+    }
+
+    @Benchmark
+    public void Q6PL2SCCTest() throws Exception {
+        try (Session session = driver.session()){
+            Result result = session.run(scc);
+        }
+    }
+
+    @Benchmark
+    public void Q7PL1ProjTest() throws Exception {
+        try (Session session = driver.session()){
+            session.run(dropproj);
+            session.run(pl1proj);
+        }
+    }
+
+    @Benchmark
+    public void Q8PL1SCCTest() throws Exception {
+        try (Session session = driver.session()){
+            Result result = session.run(scc);
+        }
+    }
+
+    @Test
+    public void importG() throws Exception {
+        Application.importGraph("list_append", 60);
     }
 
     public static void main(String[] args) throws Exception {
@@ -100,15 +196,15 @@ public class CypherBenchmarkTest {
             System.exit(0);
         }
         int max = 200;
-        for (int i = 10; i <= max; i+= 10) {
+        for (int i = 10; i <= 20; i+= 10) {
             Application.importGraph(args[0], i);
             final Options opts = new OptionsBuilder()
                     .include(CypherBenchmarkTest.class.getSimpleName())
                     .forks(1)
-                    .measurementIterations(5)
-                    .warmupIterations(5)
-                    .measurementTime(TimeValue.seconds(5))
-                    .warmupTime(TimeValue.seconds(5))
+                    .measurementIterations(1)
+                    .warmupIterations(1)
+                    .measurementTime(TimeValue.seconds(1))
+                    .warmupTime(TimeValue.seconds(1))
                     .timeUnit(TimeUnit.MILLISECONDS)
                     .verbosity(VerboseMode.SILENT)
                     .build();
